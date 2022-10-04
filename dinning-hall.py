8a171bbf950adfe2be1c37314ff973613f29e3d8
dann1kk <daniel.pogorevici@isa.utm.md>
2022-10-05 00:12:00 +0300

    add logic for serving back the order and max time

--- dinning-hall.py
+++ dinning-hall.py
@@ -11,6 +11,9 @@ from Waiters import *
 
 Orders = queue.Queue()
 Orders.join()
+Orders_stack = []
+Orders_done = []
+Order_rating = []
 Time_Unit = 1
 threads = []
 
@@ -21,6 +24,8 @@ app = Flask(__name__)
 def distribution():
     order = request.get_json()
     print(f'Received order from kitchen. Order ID: {order["order_id"]}')
+    waiter_thread: Waiter = next((w for w in threads if type(w) == Waiter and w.id == order['waiter_id']), None)
+    waiter_thread.serve_order(order)
     return {'isSuccess': True}
 
 
@@ -45,22 +50,45 @@ class Waiter(threading.Thread):
             table_id = next((i for i, table in enumerate(Tables) if table['id'] == Order['table_id']), None)
             Tables[table_id]['state'] = "waiting for the order to be served"
             print(
-                f'Order with Id:{Order["id"]} and items: {Order["items"]} picked up by: {threading.current_thread().name} from table {Order["table_id"]}')
+                f'Order with Id:{Order["id"]}, priority: {Order["priority"]} and items: {Order["items"]} picked up by: {threading.current_thread().name} from table {Order["table_id"]}')
             payload = dict({
                 'order_id': Order['id'],
                 'table_id': Order['table_id'],
                 'waiter_id': self.id,
                 'items': Order['items'],
-                'priority': Order['priority']
+                'priority': Order['priority'],
+                'max_wait': Order['max_wait'],
+                'time_start': time.time()
             })
             time.sleep(random.randint(2, 4) * Time_Unit)
 
-            requests.post('http://kitchen:80/order', json=payload, timeout=0.0000000001)
+            requests.post('http://localhost:8081/order', json=payload, timeout=0.0000000001)
 
         except (queue.Empty, requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
             pass
 
+    def serve_order(self, ordered_order):
+        received_order = next(
+            (order for i, order in enumerate(Orders_stack) if order['id'] == ordered_order['order_id']), None)
+        if received_order is not None and received_order['items'].sort() == ordered_order['items'].sort():
+            table_id = next((i for i, table in enumerate(Tables) if table['id'] == ordered_order['table_id']),
+                            None)
+            Tables[table_id]['state'] = "Free"
+            order_served = int(time.time())
+            order_picked_up = int(ordered_order['time_start'])
+            Total_order_time = order_served - order_picked_up
 
+            served_order = {**ordered_order, 'Serving_time': Total_order_time, 'status': 'DONE'}
+            Orders_done.append(served_order)
+            print( f'Order served: \n'
+                      f'Order Id: {served_order["order_id"]}\n'
+                      f'Table Id: {served_order["table_id"]}\n'
+                      f'Waiter Id: {served_order["waiter_id"]}\n'
+                      f'Items: {served_order["items"]}\n'
+                      f'Priority: {served_order["priority"]}\n'
+                      f'Max Wait: {served_order["max_wait"]}\n'
+                      f'Waiting time: {served_order["Serving_time"]}'
+                      )
 # Class for customers which extends thread
 class Customers(threading.Thread):
     def __init__(self, *args, **kwargs):
@@ -77,21 +105,30 @@ class Customers(threading.Thread):
         (table_id, table) = next(
             ((idx, table) for idx, table in enumerate(Tables) if table['state'] == "Free"), (None, None))
         if table_id is not None:
+            wait_time = 0 
             # generate the random order
             order_id = int(random.randint(1, 1000))
-            items = random.sample(range(1, 14), random.randint(1, 10))
+            items = []
+            for i in range(random.randint(1, 10)):
+                choice = random.choice(Menu)
+                if wait_time < choice['preparation-time']:
+                    wait_time = choice['preparation-time']
+                items.append(choice['id'])
+            wait_time = wait_time * 1.3
             order = {
                 'table_id': table['id'],
                 'id': order_id,
                 'items': items,
-                'priority': random.randint(1, 5)
+                'priority': random.randint(1, 5),
+                'max_wait': wait_time
             }
             Orders.put(order)
+            Orders_stack.append(order)
             Tables[table_id]['state'] = "waiting to make a order"
-
+            Tables[table_id]['order_id'] = order_id
 
 def run_dinning_hall():
-    main_thread = threading.Thread(target=lambda: app.run(host='0.0.0.0', port=80, debug=False, use_reloader=False),
+    main_thread = threading.Thread(target=lambda: app.run(host='0.0.0.0', port=8080, debug=False, use_reloader=False),
                                    daemon=True)
     main_thread.start()
     print("Dinning-hall is running!")
